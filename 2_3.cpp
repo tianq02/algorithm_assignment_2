7100222d4c6b517d1abecb1798b3ac249e7dd84a
tianq02 <ss5ithjv@anonaddy.com>
2024-11-14 17:51:40 +0800

    foobar

--- 2_3.cpp
+++ 2_3.cpp
@@ -19,37 +19,65 @@
 int numbers[10000] = {0};
 
 /**
- * get the median number in an array of numbers with length 5.
+ * get the median number by counting occurrence.
  * change lr in real world cases, e.g. last group of 5 in BFPRT.
  * beware we're not checking array boundaries here.
  * this function has no side effects, have fun multithreading.
  * average time: O(n^2), space: O(1)
  * when t<<n, average time O(n)
- * @param num5 the array you want to determine its median
- * @param l left offset, search would start at num5[l]
- * @param r right offset, search ends at num5[r]
+ * @param arr the array you want to determine its median
+ * @param l left offset, search would start at arr[l]
+ * @param r right offset, search ends at arr[r]
  * @param t target rank, [0,r-l], get n-th smallest in given range, -1:median
- * @return value of median or rank t, should be int between l and r. on error return -1
+ * @return value of median or rank t, should be a number in array between l and r. on error return -1
  */
-int Median5(const int num5[], int l = 0, int r = 4, int t = -1);
+int CountedNth(const int arr[], int l = 0, int r = 4, int t = -1);
 
 /**
- * Another implementation of median_in_five
+ * Another implementation of median.
  * It performs bubble sort on given range of array to get median number, leaving some side effect
  * average time: O(n^2), space: O(1)
- * @param num5 the array you want to determine its median
- * @param l left offset, search would start at num5[l]
- * @param r right offset, search ends at num5[r]
+ * @param arr the array you want to determine its median
+ * @param l left offset, search would start at arr[l]
+ * @param r right offset, search ends at arr[r]
  * @param t target rank, [0,r-l], get n-th smallest in given range, -1:median
- * @return value of median or rank t, should be int between l and r. on error return -1
+ * @return value of median or rank t, should be a number in array between l and r. on error return -1
  */
-int Sorted5(int num5[], int l = 0, int r = 4, int t = -1);
+int SortedNth(int arr[], int l = 0, int r = 4, int t = -1);
 
-int Partition(int array[], int l, int r, int pivot);
+/**
+ * get median using std sort.
+ * @param arr the array you want to determine its median
+ * @param l left offset, search would start at arr[l]
+ * @param r right offset, search ends at arr[r]
+ * @param t target rank, [0,r-l], get n-th smallest in given range, -1:median
+ * @return value of median or rank t, should be a number in array between l and r. on error return -1
+ */
+int StdSortedNth(int arr[], int l = 0, int r = 4, int t = -1);
 
-auto ComputeMedian = Median5;
+/**
+ * wrapper for bruteforce nth
+ * @param arr the array you want to determine its median
+ * @param l left offset, search would start at arr[l]
+ * @param r right offset, search ends at arr[r]
+ * @param t target rank, [0,r-l], get n-th smallest in given range, -1:median
+ * @return value of median or rank t, should be a number in array between l and r. on error return -1
+ */
+int BruteNth(int arr[], int l = 0, int r = 4, int t = -1) {
+    return SortedNth(arr, l, r, t);
+}
 
-int Median5(const int num5[], const int l, const int r, const int t) {
+/**
+ * get median in given arr
+ * wrap up BruteNth, calculate targeted median index, if duplicated, return their average
+ * @param arr the array you want to determine its median
+ * @param l left offset, search would start at arr[l]
+ * @param r right offset, search ends at arr[r]
+ * @return median number
+ */
+double BruteMedian(int arr[], int l = 0, int r = 4);
+
+int CountedNth(const int arr[], const int l, const int r, const int t) {
     // idea: find smallest each time until desired rank.
     if (l > r || t > r - l) return -1;
     const int targetRank = t < 0 ? (l + r) / 2 : t;
@@ -59,10 +87,10 @@ int Median5(const int num5[], const int l, const int r, const int t) {
     while (curRanked < targetRank + 1) {
         int newMin = INT_MAX, newRanked = 0;
         for (int i = l; i <= r; i++) {
-            if (num5[i] > lastMin & num5[i] < newMin) {
-                newMin = num5[i];
+            if (arr[i] > lastMin & arr[i] < newMin) {
+                newMin = arr[i];
                 newRanked = 1;
-            } else if (num5[i] == newMin) {
+            } else if (arr[i] == newMin) {
                 newRanked++;
             }
         }
@@ -74,20 +102,41 @@ int Median5(const int num5[], const int l, const int r, const int t) {
     return lastMin;
 }
 
-int Sorted5(int num5[], const int l, const int r, const int t) {
+int SortedNth(int arr[], const int l, const int r, const int t) {
     if (l > r || t > r - l) return -1;
     for (int i = l; i <= r; i++) {
         for (int j = i; j <= r; j++) {
-            if (num5[i] > num5[j]) {
-                int temp = num5[i];
-                num5[i] = num5[j];
-                num5[j] = temp;
+            if (arr[i] > arr[j]) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    if (t < 0) return num5[(l + r) / 2];
-    return num5[t];
+    if (t < 0) return arr[(l + r) / 2];
+    return arr[t];
 }
+
+int StdSortedNth(int arr[], const int l, const int r, const int t) {
+    if (l > r || t > r - l) return -1;
+    std::sort(arr+l, arr + r);
+    if (t < 0) return arr[(l + r) / 2];
+    return arr[t];
+}
+
+double BruteMedian(int arr[], const int l, const int r) {
+    if (l > r) return -1;
+    const int midL = (l + r) / 2;
+    const int midR = (l + r + 1) / 2;
+
+    if (midL == midR)
+        return BruteNth(arr, l, r, midL);
+
+    return (static_cast<double>(BruteNth(arr, l, r, midL)) + static_cast<double>(BruteNth(arr, l, r, midR)))/2;
+}
+
+
+
 
 
 int main() {
@@ -102,9 +151,9 @@ int main() {
         std::cout << i << ":\t" << numbers[i] << std::endl;
     }
     std::cout << std::endl;
-    const int median1 = Median5(numbers, 0, 99);
-    const int median2 = ComputeMedian(numbers, 0, 99, -1);
-    const int median3 = Sorted5(numbers, 0, 99);
+    const int median1 = CountedNth(numbers, 0, 99);
+    const int median2 = BruteNth(numbers, 0, 99, -1);
+    const int median3 = SortedNth(numbers, 0, 99);
     std::cout << "median1:" << median1 << std::endl;
     std::cout << "median2:" << median2 << std::endl;
     std::cout << "median3:" << median3 << std::endl;
