70755a663d2a86955d2f667cca2834e941228e40
tianq02 <ss5ithjv@anonaddy.com>
2024-11-26 17:43:51 +0800

    add template magic

--- 2_3.cpp
+++ 2_3.cpp
@@ -14,10 +14,10 @@
 
 #include <iostream>
 #include <random>  // 为了在PCG算法中使用C11风格的 random device 安全初始化
-#include <climits>  // 删了这行，debian上的g++不认INT_MIN
+#include <limits>  // 在CountedNth中使用，确定对应类型的最小值和最大值
 #include "pcg_random.hpp"  // 非常优秀的现代伪随机生成器
 
-int numbers[10000] = {0};
+double numbers[10000] = {0};
 
 /**
  * get the median number by counting occurrence.
@@ -32,7 +32,8 @@ int numbers[10000] = {0};
  * @param t target rank, [0,r-l], get n-th smallest in given range, -1:median
  * @return value of median or rank t, should be a number in array between l and r. on error return -1
  */
-int CountedNth(const int arr[], int l = 0, int r = 4, int t = -1);
+template<typename NumType>
+NumType CountedNth(const NumType arr[], int l = 0, int r = 4, int t = -1);
 
 /**
  * Another implementation of median.
@@ -55,7 +56,8 @@ NumType SortedNth(NumType arr[], int l = 0, int r = 4, int t = -1);
  * @param t target rank, [0,r-l], get n-th smallest in given range, -1:median
  * @return value of median or rank t, should be a number in array between l and r. on error return -1
  */
-int StdSortedNth(int arr[], int l = 0, int r = 4, int t = -1);
+template<typename NumType>
+NumType StdSortedNth(NumType arr[], int l = 0, int r = 4, int t = -1);
 
 /**
  * wrapper for bruteforce nth
@@ -66,7 +68,7 @@ int StdSortedNth(int arr[], int l = 0, int r = 4, int t = -1);
  * @return value of median or rank t, should be a number in array between l and r. on error return -1
  */
 template<typename NumType>
-int BruteNth(NumType arr[], int l = 0, int r = 4, int t = -1) {
+NumType BruteNth(NumType arr[], int l = 0, int r = 4, int t = -1) {
     return SortedNth(arr, l, r, t);
 }
 
@@ -81,15 +83,17 @@ int BruteNth(NumType arr[], int l = 0, int r = 4, int t = -1) {
 template<typename NumType>
 NumType BruteMedian(NumType arr[], int l = 0, int r = 4);
 
-int CountedNth(const int arr[], const int l, const int r, const int t) {
+template<typename NumType>
+NumType CountedNth(const NumType arr[], const int l, const int r, const int t) {
     // idea: find smallest each time until desired rank.
-    if (l > r || t > r - l) return -1;
+    if (l > r || t > r - l) return NumType(); // Return default value of T on error
     const int targetRank = t < 0 ? (l + r) / 2 : t;
-    std::cout << "TargetRank:" << targetRank << "(" << targetRank + 1 << "th ranked)" << std::endl;
-    int lastMin = INT_MIN, curRanked = 0;
+    // std::cout << "TargetRank:" << targetRank << "(" << targetRank + 1 << "th ranked)" << std::endl;
+    NumType lastMin = std::numeric_limits<NumType>::min(), curRanked = 0;
     // +1是因为targetRank从0开始，而curRanked是计数从1开始
     while (curRanked < targetRank + 1) {
-        int newMin = INT_MAX, newRanked = 0;
+        NumType newMin = std::numeric_limits<NumType>::max();
+        int newRanked = 0;
         for (int i = l; i <= r; i++) {
             if (arr[i] > lastMin & arr[i] < newMin) {
                 newMin = arr[i];
@@ -100,8 +104,8 @@ int CountedNth(const int arr[], const int l, const int r, const int t) {
         }
         curRanked += newRanked;
         lastMin = newMin;
-        std::cout << "CurRanked:" << curRanked << "\tNewRanked:" << newRanked << "\tValue:" << newMin << std::endl;
-    }
+        // std::cout << "CurRanked:" << curRanked << "\tNewRanked:" << newRanked << "\tValue:" << newMin << std::endl;
+    }git add *
 
     return lastMin;
 }
@@ -121,9 +125,10 @@ NumType SortedNth(NumType arr[], const int l, const int r, const int t) {
     return arr[t];
 }
 
-int StdSortedNth(int arr[], const int l, const int r, const int t) {
+template<typename NumType>
+NumType StdSortedNth(NumType arr[], const int l, const int r, const int t) {
     if (l > r || t > r - l) return -1;
-    std::sort(arr+l, arr + r);
+    std::sort(arr + l, arr + r);
     if (t < 0) return arr[(l + r) / 2];
     return arr[t];
 }
@@ -137,9 +142,8 @@ NumType BruteMedian(NumType arr[], const int l, const int r) {
     if (midL == midR)
         return BruteNth(arr, l, r, midL);
 
-    return (static_cast<NumType>(BruteNth(arr, l, r, midL)) + static_cast<NumType>(BruteNth(arr, l, r, midR)))/2;
+    return (BruteNth(arr, l, r, midL) + BruteNth(arr, l, r, midR)) / NumType(2);
 }
-
 
 
 // int main() {
@@ -168,6 +172,8 @@ NumType BruteMedian(NumType arr[], const int l, const int r) {
 // }
 
 int main() {
+
+    std::cout.precision(4);
     // PCG RNG magic, see https://www.pcg-random.org/using-pcg-cpp.htm
     pcg_extras::seed_seq_from<std::random_device> seed_source;
     pcg32 rng(seed_source);
@@ -177,33 +183,30 @@ int main() {
     for (int i = 0; i < 10; i++) {
         numbers[i] = uniform_dist(rng);
         // std::cout << i << ":\t" << numbers[i] << std::endl;
-        std::cout << numbers[i] << ",\t";
+        std::cout << numbers[i] << "/ ";
     }
     std::cout << std::endl;
-    // const int median1 = CountedNth(numbers, 0, 9);
-    // const int median2 = BruteNth(numbers, 0, 9);
+    const double median1 = CountedNth(numbers, 0, 9);
+    const double median2 = BruteNth(numbers, 0, 9);
     const double median3 = SortedNth(numbers, 0, 9);
     const double median4 = BruteMedian(numbers, 0, 9);
-    // std::cout << "median1:" << median1 << std::endl;
-    // std::cout << "median2:" << median2 << std::endl;
+    std::cout << "median1:" << median1 << std::endl;
+    std::cout << "median2:" << median2 << std::endl;
     std::cout << "median3:" << median3 << std::endl;
     std::cout << "median4:" << median4 << std::endl;
     for (int i = 0; i < 10; i++)
         // std::cout << i << ":\t" << numbers[i] << std::endl;
-        std::cout << numbers[i] << ",\t";
+        std::cout << numbers[i] << "/ ";
     return 0;
-
 }
 
 /**
-* 下面时早期一些不成熟的想法，有明显的错误，删了可惜，就留在这里了
-*
-* (这样求不出中位数)
-* 为了得到O(n)时间，没法执行排序算法，将算法分成两步骤执行
-* step. 1
-* 维护左右边界min/max以及各自计数cl,cr,ck
-* 其中，cl是过小被丢弃的数量，cr过大，ck当前选中*
-*
-* 维护长k的队列s，不满时直接入队，满时和队列各元素比较并挤走一部分
-*
-*/
+ * BFPRT算法：
+ * 相当于快速选择算法，但不同的是每次选择近似中位数作为轴值
+ * 为了选择中位数，我们将数据每5个分组，分别计算中位数，并取这些中位数们的中位数（递归BFPRT）
+ * 让我们来看一下如何实现这个算法：
+ * 1. BFPRT函数： 输入可比较的数组和目标序号n，输出第n大的元素元素
+ *    其中
+ *    - 每次选择中位数作为轴值，将数组划分为两块，判断目标所处的区间，选择对应的区间进行下一步BFPRT定位
+ *    - 在选择中位数时，将数组每5个元素分块各自取中位数
+ */
